e759d8df09ae187b127f569f1402a9cd1a53ad37
Grieshko <grieshko@blacksheep.email>
2017-08-19 16:34:08 +0000

    Update Nessus files

--- nessus/getNessusPolicies.py
+++ nessus/getNessusPolicies.py
@@ -5,8 +5,8 @@ import requests
 # SETTINGS                              #
 #########################################
 nessus_url = 'https://127.0.0.1:8834'
-access_key = '232c091c80bfe6bec86906dc4b00ce55e926ab7537236e31b1ffc93ee2939f8a'
-secret_key = '3084e3c5dcdf8786178b12955b11dc3b13e5554377410587e48c9e54590d35dc'
+access_key = 'ACCESS_KEY created in the Nessus interface'
+secret_key = 'SECRET_KEY created in the Nessus interface'
 #########################################
 headers = {'Content-type': 'application/json', 'X-ApiKeys': 'accessKey='+access_key+'; secretKey='+secret_key}
 requests.packages.urllib3.disable_warnings()
